29d0e3744d688fb9526fa5e8757f6df411d16035
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2024-02-07 23:01:08 -0300

    Task #5 - desafio/aula 2 retirando pasta .vs

--- src/RocketseatAuction.API/obj/Debug/net8.0/RocketseatAuction.API.AssemblyInfo.cs
+++ src/RocketseatAuction.API/obj/Debug/net8.0/RocketseatAuction.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a7efa5df63d427b3be4340a3464b8e6833b2b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7682a0e0dfdb5f785b75d68610b28d9b9183d061")]
 [assembly: System.Reflection.AssemblyProductAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
